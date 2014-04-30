e32cf0a01e3fa25cf70b2787491968016efed525
akvartek <akvartek@bu.edu>
2014-04-30 15:53:19 -0400

    Create RandomorNew.cpp

---
+++ RandomorNew.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <string> //needed this
+#include <time.h> //and this
+
+using namespace std;
+
+int number_of_files = 0;
+
+int main(){
+	cout << "Enter 's' to select random recipe:";
+	char start;
+	cin >> start;
+	if(start == 's'){
+		srand(time(NULL));
+		string line;
+		int k;
+		int number_of_files = 4;//this will not stay at 4
+		//find a way to count the files available
+		//or just have it keep guessing until it gets a hit
+		char filename[16];
+		sprintf(filename,"recipe%i.txt",rand()%number_of_files + 1);
+		// cout<<filename<<endl;
+
+		ifstream myfile(filename);
+		while (getline(myfile,line)){
+			cout << line << '\n';
+		}
+		
+		myfile.close();
+
+	}
+
+	else if (start == 'n'){//for a new recipe
+		new_recipe();
+	}
+	//system("pause");
+	return 0;
+}
+
+int new_recipe(){
+	int x = number_of_files + 1; //still need to be able to count files
+	ofstream myfile ("recipe%x.txt");
+	string recipe;
+	cout << "Write your Recipe here." << endl;
+	cout << "Enter key when done. We'll fix this later" << endl;
+	cin >> recipe;
+  if (myfile.is_open())
+  {
+    myfile << recipe;
+    myfile.close();
+  }
+}
